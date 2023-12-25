db7b177dda6f496514d0185a0fc8aa0311c6aa56
Elahe Dastan <elahe.dstn@gmail.com>
2023-12-25 17:38:43 +0000

    feat: update static hit counter tests

--- internal/hitcounter/static_test.go
+++ internal/hitcounter/static_test.go
@@ -2,6 +2,7 @@ package hitcounter_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/elahe-dastan/simplesurance-assignment/internal/hitcounter"
 )
@@ -11,9 +12,11 @@ var _ hitcounter.HitCounter = new(hitcounter.StaticHitCounter)
 func TestStaticHitCounter(t *testing.T) {
 	hc := hitcounter.NewStatic()
 
-	hc.Hit(0)
-	hc.Hit(10)
-	hc.Hit(30)
+	now := time.Now().Unix()
+
+	hc.Hit(now)
+	hc.Hit(now + 10)
+	hc.Hit(now + 30)
 
 	if hc.Count() != 3 {
 		t.Fatalf("number of hits is %d which is not equal to %d", hc.Count(), 3)

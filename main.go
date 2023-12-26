c69686d399686b8cf0947d5d35d2207cd76a855a
Elahe Dastan <elahe.dstn@gmail.com>
2023-12-26 14:22:16 +0000

    feat: use configuration in main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/elahe-dastan/simplesurance-assignment/internal/config"
 	"github.com/elahe-dastan/simplesurance-assignment/internal/hitcounter"
 	"github.com/elahe-dastan/simplesurance-assignment/internal/http"
 )
@@ -18,29 +19,30 @@ const (
  \___|_|\__,_|\__,_|
 
 `
-	fn = "state.json"
 )
 
 func main() {
 	fmt.Print(banner)
 
+	cfg := config.New()
+
 	// Read HitCounter data from disk or make an empty HitCounter if didn't find a file
-	hc, err := hitcounter.FromFileStatic(fn)
+	hc, err := hitcounter.FromFileStatic(cfg.Filename)
 	if err != nil {
-		log.Printf("cannot read hit counter from %s %s", fn, err)
+		log.Printf("cannot read hit counter from %s %s", cfg.Filename, err)
 
 		hc = hitcounter.NewStatic()
 	}
 
 	// Save HitCounter data to file regularly
-	tick := time.NewTicker(10 * time.Second)
+	tick := time.NewTicker(cfg.WriteInterval)
 	defer tick.Stop()
 
 	go func() {
 		for {
 			<-tick.C
 
-			_ = hitcounter.ToFileStatic(fn, hc)
+			_ = hitcounter.ToFileStatic(cfg.Filename, hc)
 		}
 	}()
 

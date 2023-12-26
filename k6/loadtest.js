160b1d7b5bbbb95b33fa340af0523fa0062c40f5
Elahe Dastan <elahe.dstn@gmail.com>
2023-12-26 18:50:47 +0000

    feat: add check into the k6 script

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -1,15 +1,17 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep, check } from "k6";
 
 export let options = {
-    vus: 10, // Number of virtual users
-    duration: '30s', // Duration of the test
+  vus: 10, // Number of virtual users
+  duration: "30s", // Duration of the test
 };
 
 export default function () {
-    // Make a GET request to the server
-    http.get('http://localhost:1378/');
+  let res = http.get("http://127.0.0.1:1378/");
 
-    // Optionally, pause for a short time
-    sleep(1);
+  check(res, {
+    success: (res) => res.status == 200,
+  });
+
+  sleep(1);
 }
